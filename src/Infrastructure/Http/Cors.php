80373151a393f25a246a8f033fcbb3d5f9c75b81
Jim Guillaume <jimguillaume99@gmail.com>
2026-05-27 20:31:04 +0200

    Updated CORS to correspond to Vue

--- src/Infrastructure/Http/Cors.php
+++ src/Infrastructure/Http/Cors.php
@@ -8,8 +8,6 @@ final class Cors
     private const ALLOWED_ORIGINS = [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://localhost:4173',
-        'http://127.0.0.1:4173',
     ];
 
     public static function apply(): void
